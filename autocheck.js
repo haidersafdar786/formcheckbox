5b092c66f46055c06fdef70ba893b7e04a75e64c
haidersafdar786 <engineerhaidersafdar@gmail.com>
2025-05-12 06:39:06 -0400

    Add files via upload

---
+++ autocheck.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const interval = setInterval(() => {
+    const checkbox = document.querySelector('input[type="checkbox"]');
+    if (checkbox) {
+      checkbox.checked = true;
+      checkbox.dispatchEvent(new Event("change"));
+      clearInterval(interval);
+    }
+  }, 500);
+});
